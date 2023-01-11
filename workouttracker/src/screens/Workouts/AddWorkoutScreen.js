96108fc41834abdd4c0c0d22b220ca0824d1ca14
Roy Gero <roy.gero.ii@gmail.com>
2023-01-10 20:02:54 -0500

    Adding State for the AddWorkout stuff.

--- workouttracker/src/screens/Workouts/AddWorkoutScreen.js
+++ workouttracker/src/screens/Workouts/AddWorkoutScreen.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
-// Simplest screen to use.
-
 const AddWorkoutScreen = () => {
+
+    let todaysDate = new Date();
+    const [date, setDate] = useState(todaysDate.toLocaleDateString())
+    const [startTime, setStartTime] = useState(todaysDate.getTime()); // Replace this so it's a comprehendable string.
+    const [endTime, setEndTime] = useState("");
+    const [notes, setNotes] = useState("");
+    const [exerciseList, setExerciseList] = useState([]);
+    
     return (
         <View>
             <Text>Add Workout</Text>
             <Text>Date</Text>
-            <Text>Start Time</Text>
+            <Text>{date}</Text>
+            <Text>Start Time - {startTime}</Text>
             <Text>End Time</Text>
             <Text>Notes</Text>
             <Text>Exercise List (where you can add stuff)</Text>
