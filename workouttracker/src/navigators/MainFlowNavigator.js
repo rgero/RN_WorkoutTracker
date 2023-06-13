e12b73dd562a10b9e636688b257143eb96cace33
Roy Gero <roy.gero.ii@gmail.com>
2023-06-13 19:24:42 -0400

    Adding the Display User to the top of the Drawer.

--- workouttracker/src/navigators/MainFlowNavigator.js
+++ workouttracker/src/navigators/MainFlowNavigator.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import {createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem} from '@react-navigation/drawer';
+import { Divider } from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
 import WorkoutListScreen from '../screens/Workouts/WorkoutListScreen';
 import AddWorkoutScreen from '../screens/Workouts/AddWorkoutScreen';
 
 import {Context as AuthContext} from '../context/AuthContext';
 
 export default DrawerNavigator = () => {
-  const {signOut} = React.useContext(AuthContext);
+  const {state, signOut, tryLocalSignin} = React.useContext(AuthContext);
+
+  React.useEffect(()=> {
+    const processUser = async () => {
+        tryLocalSignin();
+    }
+    processUser();
+  }, [])
   
   const CustomDrawerContent = (props) => {
     return (
       <DrawerContentScrollView {...props}>
+          { state.displayName ? (
+            <View style={styles.displayName}>
+              <DrawerItem label={state.displayName} />
+              <Divider/>
+            </View>
+          ): (null)}
           <DrawerItemList {...props} />
           <DrawerItem label="Logout" onPress={() => signOut()} />
       </DrawerContentScrollView>
@@ -24,3 +39,8 @@ export default DrawerNavigator = () => {
     </Drawer.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  displayName: {
+  }
+})
